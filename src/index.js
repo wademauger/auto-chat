6f71f2ca664e2d04987c14e35c72ea67be5f02f0
Wade Mauger <wade_mauger@intuit.com>
2016-12-11 23:49:16 -0800

    Update consts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const webdriver = require('selenium-webdriver');
 const { By, until } = webdriver;
+const config = require('./config');
 
 const driver = new webdriver.Builder()
   .forBrowser('chrome')
   .build();
 
-driver.get('https://www.facebook.com');
-driver.findElement(By.name('q')).sendKeys('webdriver');
-driver.findElement(By.name('btnG')).click();
-driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.get('https://www.messenger.com');
+driver.findElement(By.name('email')).sendKeys(config.email);
+driver.findElement(By.name('pass')).sendKeys(config.pass);
+driver.findElement(By.id('loginbutton')).click();
+driver.wait(until.titleIs('Messenger!'), 1000);
 driver.quit();
 
